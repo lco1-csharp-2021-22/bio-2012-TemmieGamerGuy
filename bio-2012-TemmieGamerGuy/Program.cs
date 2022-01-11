9e0d141d9e8719947a2011eb9f880e33558a8607
TemmieGamerGuy <76136819+TemmieGamerGuy@users.noreply.github.com>
2022-01-11 07:18:00 +0000

    improvements

--- bio-2012-TemmieGamerGuy/Program.cs
+++ bio-2012-TemmieGamerGuy/Program.cs
@@ -5,7 +5,7 @@ namespace ValueTuplesExample
 {
 	class Program
 	{
-		public static string Trains(string flips, string pos, int limit) {
+		public static string Trains(string flips, string pos, int limit, bool print = false, char stoppoint = '/') {
 			Dictionary<char, List<char>> points = new Dictionary<char, List<char>>() { {'A',new List<char>(){'D','E','F','1'}},{'B',new List<char>(){'C','G','H','1'}},{'C',new List<char>(){'B','I','J','1'}},{'D',new List<char>(){'A','K','L','1'}},{'E',new List<char>(){'A','M','N','1'}},{'F',new List<char>(){'A','N','O','1'}},
 				{'G',new List<char>(){'B','O','P','1'}},{'H',new List<char>(){'B','P','Q','1'}},{'I',new List<char>(){'C','Q','R','1'}},{'J',new List<char>(){'C','R','S','1'}},{'K',new List<char>(){'D','S','T','1'}},
 				{'L',new List<char>(){'D','T','M','1'}},{'M',new List<char>(){'U','L','E','1'}},{'N',new List<char>(){'U','E','F','1'}},{'O',new List<char>(){'V','F','G','1'}},{'P',new List<char>(){'V','G','H','1'}},
@@ -17,8 +17,10 @@ namespace ValueTuplesExample
 
 			for (int i = 0; i < limit; i++)
 			{
+				if (print) Console.WriteLine(here);
 				int entry = points[next].IndexOf(here);
 				here = next;
+				if (stoppoint != '/' && here == stoppoint) break;
 				int state = points[here][3] - 48;
 				if (state > 3)
 				{
@@ -36,17 +38,13 @@ namespace ValueTuplesExample
 		}
         static void Main(string[] args)
         {
-            Dictionary<char, List<char>> points = new Dictionary<char, List<char>>() { {'A',new List<char>(){'D','E','F','1'}},{'B',new List<char>(){'C','G','H','1'}},{'C',new List<char>(){'B','I','J','1'}},{'D',new List<char>(){'A','K','L','1'}},{'E',new List<char>(){'A','M','N','1'}},{'F',new List<char>(){'A','N','O','1'}},
-                {'G',new List<char>(){'B','O','P','1'}},{'H',new List<char>(){'B','P','Q','1'}},{'I',new List<char>(){'C','Q','R','1'}},{'J',new List<char>(){'C','R','S','1'}},{'K',new List<char>(){'D','S','T','1'}},
-                {'L',new List<char>(){'D','T','M','1'}},{'M',new List<char>(){'U','L','E','1'}},{'N',new List<char>(){'U','E','F','1'}},{'O',new List<char>(){'V','F','G','1'}},{'P',new List<char>(){'V','G','H','1'}},
-                {'Q',new List<char>(){'W','H','I','1'}},{'R',new List<char>(){'W','I','J','1'}},{'S',new List<char>(){'X','J','K','1'}},{'T',new List<char>(){'X','K','L','1'}},{'U',new List<char>(){'V','M','N','1'}},
-                {'V',new List<char>(){'U','O','P','1'}},{'W',new List<char>(){'X','Q','R','1'}},{'X',new List<char>(){'W','S','T','1'}},};
-
 			string flips = Console.ReadLine();
 			string pos = Console.ReadLine();
 			int limit = int.Parse(Console.ReadLine());
 			string result = Trains(flips, pos, limit);
 			Console.WriteLine(result);
+
+			// Q2
 
 		}
 
